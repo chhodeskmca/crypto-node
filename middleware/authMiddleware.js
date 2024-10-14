67c9d7ca0c3a461140da2051a2da51286317a2b3
Rahul Chhabra <chhabra.odesk.mca@gmail.com>
2024-10-14 17:12:07 +0530

    bug: origin according login fixing

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { WEB_DOMAINS, ROLE_TYPES } = require('../config');
 const User = require('../src/users/userModel');
+const { ObjectId } = require('../config/db');
 require('dotenv').config();
 
 const authenticateToken = async (req, res, next) => {
@@ -22,7 +23,7 @@ const authenticateToken = async (req, res, next) => {
 
         const domain = Object.keys(WEB_DOMAINS).find(domain => req.headers.origin.includes(domain))
         console.log('domain:', domain)
-        const user = await User.findById(decoded.id)
+        const user = await User.findOne({ _id: new ObjectId(decoded.id))
         console.log('user:', user)
 
         console.log('domain !== WEB_DOMAINS.localhost:', domain !== WEB_DOMAINS.localhost)
